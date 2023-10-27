356c119cee4239213b1991affec9718be952d0ca
elcanseyfullayev <147069247+elcanseyfullayev@users.noreply.github.com>
2023-10-27 04:09:38 +0400

    task

---
+++ task/assets/script.js
@@ -0,0 +1,70 @@
+// 16
+
+// function num(number) {
+//     let bolen = [0];
+//     for (let i = 0; i <= number; i++) {
+//         if (number % i ===0) {
+//             bolen.push(i);
+//         }
+//     }
+//     return bolen
+// }
+
+// console.log(num(32));
+
+// 17
+
+// function num(number) {
+//     let bolen = [0];
+//     for (let i = 0; i <= number; i++) {
+//         if (number % i ===0) {
+//             bolen.push(i);
+//         }
+//     }
+//     return bolen
+// }
+
+// console.log(num(32).length);
+
+// 18
+
+// let sum = 0
+// let multip = 1
+// function num(number) {
+//     let bolen = []
+
+//     for (let i = 0; i <= number; i++) {
+//         if (number % i === 0) {
+//             multip *= i
+//             sum += i
+//         }
+//     }
+//     return bolen;
+
+// }
+
+// num(23);
+// console.log(sum);
+// console.log(multip);
+
+
+
+// 20
+
+
+// function num(number) {
+//     let sade = true;
+//     for (let i = 2; i < number; i++) {
+//         if (number % i === 0) {
+//             return "murekkeb"
+//         }
+//         else if (sade) {
+//             return "sade"
+//         }
+//     }
+    
+
+// }
+// console.log(num(9));
+
+
